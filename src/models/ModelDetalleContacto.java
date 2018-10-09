744cb27737c685ace4fac29983e8f864c2e1b843
MarioAlbertoNL1799 <37378931+MarioAlbertoNL1799@users.noreply.github.com>
2018-10-09 14:53:20 -0500

    Se crea la variable telefono con sus metodos de get y set para poder almacenar el dato en estas variables y pueda mostrarse el numero en la vista

--- src/models/ModelDetalleContacto.java
+++ src/models/ModelDetalleContacto.java
@@ -12,6 +12,7 @@ package models;
 public class ModelDetalleContacto {
     private String nombre;
     private String email;
+    private String telefono;
 
     public String getNombre() {
         return nombre;
@@ -27,6 +28,14 @@ public class ModelDetalleContacto {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
     }
 
 }
